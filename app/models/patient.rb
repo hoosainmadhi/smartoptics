88538616b70bb139f257154701f98e7161e538f3
Hoosain Madhi <hoosain.madhi@gmail.com>
2015-03-19 10:03:56 +0200

    Nothing special

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,5 +1,16 @@
 class Patient < ActiveRecord::Base
   attr_accessible :city, :medical_aid, :medical_aid_number, :patient_name, :patient_number, :postal_address, :postal_code, :principal_member, :tel_no
   has_many :consults # A patient can go for one or many consults
-  #accepts_nested_attributes_for :consults #so we can manage Consults through Patients - needed to display multiple models per form
+  accepts_nested_attributes_for :consults #so we can manage Consults through Patients - needed to display multiple models per form
 end
+
+#Nested Forms Notes
+#form will submit the main object (e.g. the Patient) as normal 
+#but it will sneak in a bunch of attributes for the other object(s) 
+#you want to create (e.g. Consult object(s))
+
+#You will need to prepare the User model so that it knows to create 
+#one or more ShippingAddress objects if it receives their attributes 
+#when creating a Patient. 
+#This is done by adding a method to your Patient model 
+#called #accepts_nested_attributes_for which accepts the name of an association
