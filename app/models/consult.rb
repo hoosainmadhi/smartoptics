d7ce1c965197742268cede8c0bf58183a7e071f9
Hoosain Madhi <hoosain.madhi@gmail.com>
2015-03-21 09:42:14 +0200

    Rails Cast Tutorial

--- app/models/consult.rb
+++ app/models/consult.rb
@@ -1,4 +1,4 @@
 class Consult < ActiveRecord::Base
   attr_accessible :patient_id, :reservation_number, :service_date
-  belongs_to :patient # A consult is unique to a patient 
+  belongs_to :patient, :dependent => :destroy # A consult is unique to a patient 
 end
--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,5 +1,7 @@
 class Patient < ActiveRecord::Base
   attr_accessible :city, :medical_aid, :medical_aid_number, :patient_name, :patient_number, :postal_address, :postal_code, :principal_member, :tel_no
   has_many :consults # A patient can go for one or many consults
-  #accepts_nested_attributes_for :consults #so we can manage Consults through Patients - needed to display multiple models per form
+  accepts_nested_attributes_for :consults,  #so we can manage Consults through Patients - needed to display multiple models per form
+                                :reject_if => lambda { |a| a[:content].blank? },
+                                :allow_destroy => true
 end
