8cf82dd14236844f249bd38adbc2138974015114
Hoosain Madhi <hoosain.madhi@gmail.com>
2015-03-23 17:10:03 +0200

    Working version of "Remove" Dispense Item

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,7 +1,7 @@
 class Patient < ActiveRecord::Base
   attr_accessible   :city, :medical_aid, :medical_aid_number, :patient_name, :patient_number, :postal_address, :postal_code, :principal_member, :tel_no, :consults_attributes, :dispenses_attributes
   has_many :consults, :dependent => :destroy # A patient can go for one or many consults
-  accepts_nested_attributes_for :consults #so we can manage Consults through Patients - needed to display multiple models per form
+  accepts_nested_attributes_for :consults, :allow_destroy => true #so we can manage Consults through Patients - needed to display multiple models per form
 end
 
 #Nested Forms Notes
